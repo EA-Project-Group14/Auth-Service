634fe6cce83be8737918a9a3497ec4b0b30d24e9
yousefsalem <yousef_salem97@yahoo.com>
2022-09-23 17:25:22 -0500

    add eureka client

--- src/main/java/ea/project/authservice/AuthServiceApplication.java
+++ src/main/java/ea/project/authservice/AuthServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 //import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,8 +12,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 @EnableWebSecurity
-//@EnableEurekaClient
-//@EnableFeignClients
+@EnableEurekaClient
 public class AuthServiceApplication {
 
     public static void main(String[] args) {
